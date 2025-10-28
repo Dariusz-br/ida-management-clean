6d96a7cdd88365dc8f0b5d9705e9a403770fe32f
Dariusz-br <dariusz.akim@gmail.com>
2025-10-28 03:30:02 -0300

    layout change

--- .next/static/chunks/app/ida-management/layout-bc0490723e60abff.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[186],{6885:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,6754,23)),Promise.resolve().then(l.t.bind(l,7226,23))},7226:()=>{},6754:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}}},e=>{var s=s=>e(e.s=s);e.O(0,[805,130,215,744],()=>s(6885)),_N_E=e.O()}]);
